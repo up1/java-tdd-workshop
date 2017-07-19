30d5c5119faf7b3dc1c121f7445ea9772aa7fdf9
somkiat <somkiat.p@gmail.com>
2017-07-19 14:39:08 +0700

    Range workshop

---
+++ range/src/main/java/MyRange.java
@@ -0,0 +1,21 @@
+public class MyRange {
+    public String of(String input) {
+        return null;
+    }
+
+    public int getLastNumber(String input) {
+        return 0;
+    }
+
+    public boolean isก้ามปูปิด(String input) {
+        return false;
+    }
+
+    public int getFirstNumber(String input) {
+        return 0;
+    }
+
+    public boolean isก้ามปูเปิด(String input) {
+        return false;
+    }
+}
